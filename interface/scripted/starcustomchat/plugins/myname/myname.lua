905c30899e8079b62b3c630aab7f760ebbb086c2
Degranon <boba00099@gmail.com>
2025-06-15 15:56:09 +0200

    My Name plugin

---
+++ interface/scripted/starcustomchat/plugins/myname/myname.lua
@@ -0,0 +1,84 @@
+require "/interface/scripted/starcustomchat/plugin.lua"
+
+myname = PluginClass:new(
+  { name = "myname" }
+)
+
+function myname:init(chat)
+  PluginClass.init(self, chat)
+
+  self.myNameList = player.getProperty("scc_myname_list") or {}
+  self.coloringEnabled = root.getConfiguration("coloringscc_myname_coloring_enabled") or false
+  self.pingEnabled = root.getConfiguration("scc_myname_ping_enabled") or false
+  self.backgroundEnabled = root.getConfiguration("scc_myname_background_enabled") or false
+  table.insert(self.myNameList, player.name())
+
+  self:populateMessagesToHighlight()
+end
+
+function myname:populateMessagesToHighlight()
+  self.highlightMessages = {}
+  for _, message in ipairs(self.customChat.messages) do
+    if message.myNameToHighlight then
+      table.insert(self.highlightMessages, message)
+    end
+  end
+end
+
+function myname:onSettingsUpdate()
+  self.myNameList = player.getProperty("scc_myname_list") or {}
+  table.insert(self.myNameList, player.name())
+  self.coloringEnabled = root.getConfiguration("coloringscc_myname_coloring_enabled") or false
+  self.pingEnabled = root.getConfiguration("scc_myname_ping_enabled") or false
+  self.backgroundEnabled = root.getConfiguration("scc_myname_background_enabled") or false
+end
+
+function myname:update(dt)
+  if self.backgroundEnabled then
+    for _, message in ipairs(self.highlightMessages) do
+      self.customChat:highlightMessage(message, self.customChat:getColor("mynamebackground"))
+    end
+  end
+end
+
+function myname:formatIncomingMessage(message)
+  if not message.nickname or message.nickname == player.name() then
+    return message
+  end
+
+  if not self.coloringEnabled then
+    for _, name in ipairs(self.myNameList) do
+      if message.text:find(name:lower(), nil, true) then
+        message.myNameToHighlight = true
+        table.insert(self.highlightMessages, message)
+        if self.pingEnabled then
+          pane.playSound(self.pingSound)
+          starcustomchat.utils.alert("settings.plugins.myname.name_used", message.nickname)
+        end
+        return message
+      end
+    end
+  else
+    local handled = false
+    message.text = message.text:gsub('%w+', function(word)
+      for _, name in ipairs(self.myNameList) do
+        if word:lower() == name:lower() then
+          if not handled then
+            message.myNameToHighlight = true
+            table.insert(self.highlightMessages, message)
+            if self.pingEnabled then
+              pane.playSound(self.pingSound)
+              starcustomchat.utils.alert("settings.plugins.myname.name_used", message.nickname)
+            end
+            handled = true
+          end
+          return "^set;^" .. self.customChat:getColor("myname") .. ";" .. name .. "^reset;"
+        end
+      end
+      return word
+    end)
+  end
+
+
+  return message
+end
---
+++ interface/scripted/starcustomchat/plugins/myname/mynamesettings.lua
@@ -0,0 +1,97 @@
+require "/interface/scripted/starcustomchatsettings/settingsplugin.lua"
+
+myname = SettingsPluginClass:new(
+  { name = "myname" }
+)
+
+
+-- Settings
+function myname:init()
+  self:_loadConfig()
+
+  self.myNameList = player.getProperty("scc_myname_list") or {}
+  local coloring = root.getConfiguration("coloringscc_myname_coloring_enabled") or false
+  local pingEnabled = root.getConfiguration("scc_myname_ping_enabled") or false
+  local backgroundEnabled = root.getConfiguration("scc_myname_background_enabled") or false
+
+  self.widget.setChecked("chkColoringEnabled", coloring)
+  self.widget.setChecked("chkPingEnabled", pingEnabled)
+  self.widget.setChecked("chkBackgroundEnabled", backgroundEnabled)
+end
+
+function myname:openTab()
+  self.widget.registerMemberCallback("saScrollArea.listItems", "removeName", function(_, data)
+    self:removeName(_, data)
+  end)
+  self:populateScrollArea()
+end
+
+function myname:populateScrollArea()
+  self.widget.clearListItems("saScrollArea.listItems")
+
+  -- Always starts with player name
+  local li = self.widget.addListItem("saScrollArea.listItems")
+  self.widget.setText("saScrollArea.listItems." .. li .. ".name", player.name())
+  self.widget.setButtonEnabled("saScrollArea.listItems." .. li .. ".btnRemove", false)
+
+  for _, name in ipairs(self.myNameList) do
+    local li = self.widget.addListItem("saScrollArea.listItems")
+    self.widget.setText("saScrollArea.listItems." .. li .. ".name", name)
+    self.widget.setData("saScrollArea.listItems." .. li .. ".btnRemove", {
+      displayText = "settings.plugins.myname.remove_name",
+      name = name
+    })
+  end
+
+end
+
+function myname:removeName(_, data)
+  local li = self.widget.getListSelected("saScrollArea.listItems")
+  if not li or li == 1 then
+    return
+  end
+
+  local ind = index(self.myNameList, data.name)
+  if ind then
+    table.remove(self.myNameList, ind)
+    player.setProperty("scc_myname_list", self.myNameList)
+    self:populateScrollArea()
+  end
+end
+
+function myname:addNewName()
+  local name = self.widget.getText("tbxAddNewName")
+  if name == "" then
+    return
+  end
+
+
+  local li = self.widget.addListItem("saScrollArea.listItems")
+  self.widget.setText("saScrollArea.listItems." .. li .. ".name", name)
+  self.widget.setText("tbxAddNewName", "")
+  self.widget.setData("saScrollArea.listItems." .. li .. ".btnRemove", {
+    displayText = "settings.plugins.myname.remove_name",
+    name = name
+  })
+
+  table.insert(self.myNameList, name)
+  player.setProperty("scc_myname_list", self.myNameList)
+end
+
+function myname:toggleMyNameEnabled()
+  local coloring = self.widget.getChecked("chkColoringEnabled")
+  root.setConfiguration("coloringscc_myname_coloring_enabled", coloring)
+  save()
+end
+
+function myname:togglePingEnabled()
+  local pingEnabled = self.widget.getChecked("chkPingEnabled")
+  root.setConfiguration("scc_myname_ping_enabled", pingEnabled)
+  save()
+end
+
+function myname:toggleBackgroundEnabled()
+  local backgroundEnabled = self.widget.getChecked("chkBackgroundEnabled")
+  root.setConfiguration("scc_myname_background_enabled", backgroundEnabled)
+  save()
+end
